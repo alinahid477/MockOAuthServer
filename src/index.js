5e5be774e0d6f1ca45aff3ea087855924cdae249
ali_nahid <alinahid@gmail.com>
2020-10-28 16:34:09 +1100

    jsonwebtoken added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const express = require('express');
 var url = require('url');
@@ -17,79 +18,188 @@ app.use(express.json());
 const eligible_accesses = new Set();
 const accessTokens = new Set();
 
-eligible_accesses.add({clientId:'1111', clientSecret:'secret1'});
-eligible_accesses.add({clientId:'2222', clientSecret:'secret2'});
-eligible_accesses.add({clientId:'3333', clientSecret:'secret3'});
-eligible_accesses.add({clientId:'4444', clientSecret:'secret4'});
-eligible_accesses.add({clientId:'5555', clientSecret:'secret5'});
-eligible_accesses.add({clientId:'6666', clientSecret:'secret6'});
+eligible_accesses.add({clientSecret:'secret1'});
+eligible_accesses.add({clientSecret:'secret2'});
+eligible_accesses.add({clientSecret:'secret3'});
+eligible_accesses.add({clientSecret:'secret4'});
+eligible_accesses.add({clientSecret:'secret5'});
+eligible_accesses.add({clientSecret:'secret6'});
 
 app.get('/healthcheck', (req, res) => {
     return res.status(200).json({message:'I am working... eff off'});
 });
 
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        message:'Welcome to simple OAuth Server Mock - by Ali Nahid',
+        usage:[
+            {
+                endpoint:'/healthcheck',
+                method:'GET',
+                description:'to healthcheck auth server'
+            },
+            {
+                endpoint:'/auth/token',
+                method:'POST',
+                description:'Requst Body must contain clientId, clientSecret, grantType. you can use any client id, just use a unique one. \
+                                clientSecret: must be one of these secret1,secret2,secret3,secret4,secret5,secret6 \
+                                grantType must be client_credentials',
+                sampleRequest: {
+                    clientId:'myuniqueclientid',
+                    clientSecret:'secret1',
+                    grantType:'client_credentials'
+                },
+                sampleResponse:{
+                    access_token:'generated granted token, This token is cached for 30 days unless you remove it.',
+                    expires_in:'expiry time',
+                    token_type:'Bearer'
+                }
+            },
+            {
+                endpoint:'/auth/token/remove',
+                method:'GET',
+                description:'Remove the token you have been granted. Please clean up after yourself.',
+                sampleRequest: {
+                    header: {
+                        Authorization: 'Bearer <your token>'
+                    } 
+                },
+                sampleResponse:{
+                    message:'removed successfully'
+                }
+            },
+
+            {
+                endpoint:'/receive/secure',
+                method:'POST',
+                description:'post any payload with authorization header with the granted token',
+                sampleRequest: {
+                    header: {
+                        Authorization: 'Bearer <your token, granted from /auth/token endpoint>'
+                    },
+                    body:'your request body. whatever body it is. This endpoint will output the body if authorization is successful.' 
+                },
+                sampleSuccessfulResponse:'If authorisation is successful this the response body will contain whatever body you supplied in the request payload with 200 response code.',
+                sampleFailedResponse:'If authorisation is not successful then the response will be a 400 code.'
+            },
+        ]
+    });
+});
+
 app.post('/auth/token', (req, res) => {
-    if (req.body.grantType === 'client_credentials') { 
-        for (const item of eligible_accesses) {
-            if(item.clientId === req.body.clientId && item.clientSecret === req.body.clientSecret ) {
-                const token = new Array(50).fill(null).map(() => Math.floor(Math.random() * 10)).join('');
-                redisClient.set(token, `${req.body.clientId}-${req.body.clientSecret}`);
-                return res.status(200).json({ 'access_token': token, 'expires_in': 60 * 60 * 24, 'token_type':'Bearer' });
+    try {
+        if (req.body.grantType === 'client_credentials') { 
+            for (const item of eligible_accesses) {
+                if(item.clientSecret === req.body.clientSecret ) {
+                    const generatedToken = new Array(50).fill(null).map(() => Math.floor(Math.random() * 10)).join('');
+                    const tokenExpiry = Date.now() + 60 * 20 * 1000;
+                    const tokenObject = { 'grantedToken': generatedToken }
+                    const token = jwt.sign( tokenObject, process.env.JWT_SIGNING_KEY, { expiresIn: '20m' } );
+                    redisClient.set(token, `${req.body.clientId}-${req.body.clientSecret}`);
+                    return res.status(200).json({ access_token:token, 'expires_in': tokenExpiry , 'token_type':'Bearer'});
+                }
             }
+            return res.status(400).json({ message: 'Invalid credential' });  
+            // Generate a string of 50 random digits
+            
+        } else {
+          res.status(400).json({ message: 'Invalid grant type' });
         }
-        return res.status(400).json({ message: 'Invalid credential' });  
-        // Generate a string of 50 random digits
-        
-    } else {
-      res.status(400).json({ message: 'Invalid grant type' });
+    }catch(err) {
+        console.error(err);
     }
+    
   });
 
   app.post('/receive/secure', (req, res) => {
-    let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-    if (token && token.startsWith('Bearer ')) {
-        // Remove Bearer from string
-        token = token.slice(7, token.length);
-    }
-    if (!token) {
-      return res.status(403).json({ message: 'Unauthorized' });
-    }
-    return redisClient.get(token, function (err, reply) {
-        console.log(reply);
-        if (reply != null) {
-            for (const item of eligible_accesses) {
-                if(`${item.clientId}-${item.clientSecret}` === reply) {
+      try {
+        let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
+        if (token && token.startsWith('Bearer ')) {
+            // Remove Bearer from string
+            token = token.slice(7, token.length);
+        }
+        if (!token) {
+        return res.status(403).json({ message: 'Unauthorized' });
+        }
+        return redisClient.get(token, function (err, reply) {
+            console.log("found token:",reply);
+            if (reply != null) {
+                let isValid = false;
+                try {
+                    const decoded = jwt.verify(token, process.env.JWT_SIGNING_KEY);
+                    if (decoded.grantedToken) {
+                        console.log('valid token', decoded);
+                        isValid = true;
+                    } else {
+                        console.log('token exprired', decoded);
+                    }
+                } catch (err) {
+                    console.error('failed to verify token: ',err);
+                }
+                if(isValid) {
                     return res.status(200).json(req.body);
                 }
+                redisClient.del(token);
+                return res.status(400).json({message:'authorisation denied. Token expired or cannot be verified.'});
+            } else {
+                res.status(400).json({message:'authorisation denied. You have never authorised bruh!!.'});
             }
-            return res.status(400).json({message:'authorisation denied'});
-        } else {
-            res.status(400).json({message:'authorisation denied'});
-        }
-      });
+        });
+      }catch(err) {
+          console.error(err);
+      }
+    
     
   });
 
   app.get('/auth/token/remove', (req, res) => {
-    let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-    if (token && token.startsWith('Bearer ')) {
-        // Remove Bearer from string
-        token = token.slice(7, token.length);
-    }
-    console.log("remove: ",token);
-    if (!token) {
-      return res.status(403).json({ message: 'Unauthorized' });
-    }
-    return redisClient.del(token, function (err, response) {
-        if (response === 1) {
-            return res.status(200).json({message:'removed successfully'});
-        } else {
-            return res.status(400).json({message:'cannot remove'});
+      try {
+        let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
+        if (token && token.startsWith('Bearer ')) {
+            // Remove Bearer from string
+            token = token.slice(7, token.length);
         }
-      });
+        console.log("remove: ",token);
+        if (!token) {
+        return res.status(403).json({ message: 'Unauthorized' });
+        }
+        return redisClient.del(token, function (err, response) {
+            if (response === 1) {
+                return res.status(200).json({message:'removed successfully'});
+            } else {
+                return res.status(400).json({message:'cannot remove'});
+            }
+        });
+      }catch(err) {
+          console.error(err);
+      }
+    
     
   });
 
+  app.post('/auth/token/removeall', (req,res) => {
+      try {
+        let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
+        if (token && token.startsWith('Bearer ')) {
+            // Remove Bearer from string
+            token = token.slice(7, token.length);
+        }
+        if(token !== 'aliremovesall') {
+            return res.status(400).json({message:'failed to remove all, you are not authorised for this operation'});
+        }
+        redisClient.flushdb( function (err, succeeded) {
+            console.log(succeeded); // will be true if successfull
+            if(succeeded) {
+                return res.status(200).json({message:'removed all successfully'});
+            } else {
+                return res.status(400).json({message:'failed to remove all, sorry bruh!!'});
+            }
+        });
+
+      }catch(err) {
+        console.error(err);
+      }
+  });
 
   app.listen(port, () => {
     console.log(`OAuthServer started and listening on port ${port}`);
